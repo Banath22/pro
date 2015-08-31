c2fc1695a27b30a34def2ad334a1c94635a3f92c
Banath22 <casper_vz@hotmail.com>
2015-08-31 14:53:56 +0200

    hi

--- index.php
+++ index.php
@@ -10,19 +10,19 @@
 <div class="wrapper">
 	<h1>Image gallery</h1>
 	<div class="navarrows">
-	<img id="gallery1" class="image" src="img/img0.jpg">
+	<div class="images"><img id="gallery1" class="image" src="img/img0.jpg"></div>
 		<p id="text1">1/12</p>
 		<div class="arrow" id="left1"><img id="left" src="img/arrowl.png"><p>Turn left</p></div>
 		<div class="arrow" id="right1"><img id="right" src="img/arrowr.png"><p>Turn right</p></div>
 	</div>
 	<div class="navarrows">
-	<img id="gallery2" class="image" src="img/img0.jpg">
+	<div class="images"><img id="gallery2" class="image" src="img/img0.jpg"></div>
 		<p id="text2">1/12</p>
 		<div class="arrow" id="left2"><img id="left" src="img/arrowl.png"><p>Turn left</p></div>
 		<div class="arrow" id="right2"><img id="right" src="img/arrowr.png"><p>Turn right</p></div>
 	</div>
 	<div class="navarrows">
-	<img id="gallery3" class="image" src="img/img0.jpg">
+	<div class="images"><img id="gallery3" class="image" src="img/img0.jpg"></div>
 		<p id="text3">1/12</p>
 		<div class="arrow" id="left3"><img id="left" src="img/arrowl.png"><p>Turn left</p></div>
 		<div class="arrow" id="right3"><img id="right" src="img/arrowr.png"><p>Turn right</p></div>
--- script.js
+++ script.js
@@ -14,57 +14,59 @@ window.addEventListener("load", function(){
 
 	var node = ["img0.jpg", "img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg", "img7.jpg", "img8.jpg", "img9.jpg", "img10.jpg", "img11.jpg"];
 
-	var curr_img= 0;
+	var curr_img1= 0;
+	var curr_img2= 0;
+	var curr_img3= 0;
 
 	left1.addEventListener('click',function(){
-		curr_img--;
-		if (curr_img < 0) {
-			curr_img = 11;
+		curr_img1--;
+		if (curr_img1 < 0) {
+			curr_img1 = 11;
 		};
-		img1.src="img/" + node[curr_img%12];
-        text1.innerHTML = curr_img + 1 + "/12";
+		img1.src="img/" + node[curr_img1%12];
+        text1.innerHTML = curr_img1 + 1 + "/12";
 	});
 
 	right1.addEventListener('click',function(){
-		curr_img++;
-		if (curr_img > 11) {
-			curr_img = 0;
+		curr_img1++;
+		if (curr_img1 > 11) {
+			curr_img1 = 0;
 		};
-		img1.src="img/" + node[curr_img%12];
-        text1.innerHTML = curr_img + 1 + "/12";
+		img1.src="img/" + node[curr_img1%12];
+        text1.innerHTML = curr_img1 + 1 + "/12";
 	});
 	left2.addEventListener('click',function(){
-		curr_img--;
-		if (curr_img < 0) {
-			curr_img = 11;
+		curr_img2--;
+		if (curr_img2 < 0) {
+			curr_img2 = 11;
 		};
-		img2.src="img/" + node[curr_img%12];
-        text2.innerHTML = curr_img + 1 + "/12";
+		img2.src="img/" + node[curr_img2%12];
+        text2.innerHTML = curr_img2 + 1 + "/12";
 	});
 
 	right2.addEventListener('click',function(){
-		curr_img++;
-		if (curr_img > 11) {
-			curr_img = 0;
+		curr_img2++;
+		if (curr_img2 > 11) {
+			curr_img2 = 0;
 		};
-		img2.src="img/" + node[curr_img%12];
-        text2.innerHTML = curr_img + 1 + "/12";
+		img2.src="img/" + node[curr_img2%12];
+        text2.innerHTML = curr_img2 + 1 + "/12";
 	});
 	left3.addEventListener('click',function(){
-		curr_img--;
-		if (curr_img < 0) {
-			curr_img = 11;
+		curr_img3--;
+		if (curr_img3 < 0) {
+			curr_img3 = 11;
 		};
-		img3.src="img/" + node[curr_img%12];
-        text3.innerHTML = curr_img + 1 + "/12";
+		img3.src="img/" + node[curr_img3%12];
+        text3.innerHTML = curr_img3 + 1 + "/12";
 	});
 
 	right3.addEventListener('click',function(){
-		curr_img++;
-		if (curr_img > 11) {
-			curr_img = 0;
+		curr_img3++;
+		if (curr_img3 > 11) {
+			curr_img3 = 0;
 		};
-		img3.src="img/" + node[curr_img%12];
-        text3.innerHTML = curr_img + 1 + "/12";
+		img3.src="img/" + node[curr_img3%12];
+        text3.innerHTML = curr_img3 + 1 + "/12";
 	});
 });
