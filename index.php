17a4cde91337952e8e2caaef37b618eb1b84f5f6
Banath22 <casper_vz@hotmail.com>
2015-08-31 14:20:24 +0200

    hi

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Tablet Tour</title>
+	<title>Image gallery</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script type="text/javascript" src="script.js"></script>
 	<link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
 </head>
 <body>
 <div class="wrapper">
-	<h1>Tablet Tour</h1>
+	<h1>Image gallery</h1>
 	<img id="image" class="image" src="img/img0.jpg">
 	<div class="navarrows">
 		<div class="arrow" id="left"><img id="left" src="img/arrowl.png"><p>Turn left</p></div>
