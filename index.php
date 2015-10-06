62f28801e51c971f545b59033984cc0ea8f9a1ef
ivorkill <davevanrossum@gmail.com>
2015-10-06 17:07:22 +0200

    hi

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 <head>
 	<title>Image gallery</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<script type="text/javascript" src="script.js"></script>
+	<script type="text/javascript" src="script2.js"></script>
 	<link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
 </head>
 <body>
 <div class="wrapper">
 	<h1>Image gallery</h1>
 	<div class="navarrows">
-	<div class="images"><img id="gallery1" class="image" src="img/img0.jpg"></div>
+	<div class="images"><img id="img" class="image" src="img/img0.jpg"></div>
 		<p id="text1">1/12</p>
 		<div class="arrow" id="left1"><img id="left" src="img/arrowl.png"><p>Turn left</p></div>
 		<div class="arrow" id="right1"><img id="right" src="img/arrowr.png"><p>Turn right</p></div>
